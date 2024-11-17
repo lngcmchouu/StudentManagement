398d766ca9727675084a6f76ff18f24ab6a14647
mchou <lngocmchau6704@gmail.com>
2024-11-17 12:32:39 +0700

    Add project files.

---
+++ StudentManagement/Models/StudentManagementContext.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace StudentManagement.Models;
+
+public partial class StudentManagementContext : DbContext
+{
+    private readonly string? _connectionString;
+    public StudentManagementContext()
+    {
+    }
+
+    public StudentManagementContext(DbContextOptions<StudentManagementContext> options, IConfiguration configuration)
+        : base(options)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+
+    public required DbSet<Class> Classes { get; set; }
+
+    public required DbSet<Student> Students { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Server=LAPTOP-ST2T5JNH\\SQLEXPRESS;Database=StudentManagement;User ID=sa;Password=Kthv951230.;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Class>(entity =>
+        {
+            entity.HasKey(e => e.ClassId).HasName("PK__Classes__CB1927C07368657E");
+
+            entity.Property(e => e.ClassName).HasMaxLength(50);
+        });
+
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.HasKey(e => e.StudentId).HasName("PK__Students__32C52B99F35FEC42");
+
+            entity.Property(e => e.Email).HasMaxLength(100);
+            entity.Property(e => e.FullName).HasMaxLength(100);
+            entity.Property(e => e.PhoneNumber).HasMaxLength(15);
+
+            entity.HasOne(d => d.Class).WithMany(p => p.Students)
+                .HasForeignKey(d => d.ClassId)
+                .HasConstraintName("FK__Students__ClassI__4BAC3F29");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
